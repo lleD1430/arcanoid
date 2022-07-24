63d9f4f0f937878de81acba9e51c5dac8fe80764
Anatolii <lled1430@gmail.com>
2022-07-24 19:20:15 +0300

    Create and ad data for loop

--- Arcanoid/Arcanoid/Form1.cs
+++ Arcanoid/Arcanoid/Form1.cs
@@ -12,9 +12,32 @@ namespace Arcanoid
 {
     public partial class Form1 : Form
     {
+        const int mapWidth = 16;
+        const int mapHeight = 20;
+        
+        public int[,] map = new int[mapHeight, mapWidth];
+        public int dirX = 0;
+        public int dirY = 0;
+        public int boardX;
+        public int boardY;
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        public void Init()
+        {
+            boardX = mapWidth / 2;
+            boardY = mapHeight -1;
+
+            for (int i = 0; i < mapHeight; i++)
+            {
+                for (int j = 0; j < mapWidth; j++)
+                {
+                    map[i, j] = 0;
+                }
+            }
         }
 
         private void OnPaint(object sender, PaintEventArgs e)
